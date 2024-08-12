2b62be823c92bd3b7b6cabeb5dc4791977035b53
lemursauce <lacarbo05@gmail.com>
2024-08-11 19:34:33 -0500

    start and cleanup (regards to project cards)

---
+++ src/components/projectcards/ProjectCard.js
@@ -0,0 +1,17 @@
+
+import "./projectCards.scss";
+
+function projectCard() {
+    return (
+        <li className="ProjectCard">
+            <div className="ProjectCardOutline">
+                <div className="ProjectCardContent">
+                    ...
+                </div>
+            </div>
+            
+        </li>
+    )
+}
+
+export default projectCard;
---
+++ src/components/projectcards/ProjectCards.js
@@ -0,0 +1,14 @@
+
+import { Container } from "react-bootstrap";
+import ProjectCard from "./ProjectCard"
+
+
+
+function projectCards() {
+    return (
+        <Container className="ProjectCards">
+        </Container>
+    );
+}
+
+export default projectCards;
